f04b1d4627b6ada32bfc33370c8d7f54aa9e534b
yoswic <80913704+yoswic@users.noreply.github.com>
2026-03-14 01:29:50 +0700

    Add files via upload

---
+++ fitness-sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'the-fitness-v1';
+const ASSETS = [
+  './',
+  './the-fitness.html',
+  './fitness-manifest.json',
+  './fitness-icon-192.png',
+  './fitness-icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(() => {})));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.includes('googleapis.com') || e.request.url.includes('accounts.google.com')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res && res.status === 200 && res.type !== 'opaque') {
+          caches.open(CACHE_NAME).then(c => c.put(e.request, res.clone()));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
